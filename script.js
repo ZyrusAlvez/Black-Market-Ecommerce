703446923662ac518d0398a893d01a2f2891ff14
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2025-02-08 00:14:25 +0800

    reviews: conveyer effect

---
+++ script.js
@@ -0,0 +1,74 @@
+const ids = ["minecraft", "fortnite", "valorant", "lol", "roblox", "pubg", "others"];
+document.getElementById(ids[0]).style.backgroundColor = "#FFA500";
+ids.forEach(id => {
+    document.getElementById(id).classList.add('clickable-item');
+    document.getElementById(id).onclick = function() {
+        ids.forEach(resetId => {
+            document.getElementById(resetId).style.backgroundColor = "";
+        });
+        document.getElementById(id).style.backgroundColor = "#FFA500";
+    }
+});
+
+// particles.js configuration
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#FFA500" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#FFA500" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 30, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 120,
+      color: "#FFA500",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 0.1,
+      },
+      repulse: { distance: 100, duration: 0.8 },
+      push: { particles_nb: 2 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
